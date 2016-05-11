23d23f36f63ad103bc551ea3d2ecc9bfa48bc3bd
root <indahnuralifah21@gmail.com>
2016-05-11 08:49:07 +0700

    pia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,5 @@
 
 @section('content')
 
-<h4>API (application program interface) is a set of routines, protocols, and tools for building software applications. The API specifies how software components should interact and APIs are used when programming graphical user interface (GUI) components. A good API makes it easier to develop a program by providing all the building blocks. A programmer then puts the blocks together.
-<h4>
+
 @endsection
